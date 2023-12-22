5c3955a95e127d181545dd6c73b925b7552ab206
Regis Andrade <regis.batista.andrade@outlook.com>
2023-12-21 22:45:40 -0300

    Introdução ao projeto LanchesLinux

---
+++ LanchesLinux/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using LanchesLinux.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LanchesLinux.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ LanchesLinux/Startup.cs
@@ -0,0 +1,40 @@
+﻿namespace LanchesLinux
+{
+    public class Startup
+    {
+        public IConfiguration _Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            _Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
