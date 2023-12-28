1e6f21881a8c6ff5a6bbfa0a440a14ffd2073607
Regis Andrade <regis.batista.andrade@outlook.com>
2023-12-28 20:38:45 -0300

    Inclusão de Carrosel na HomePage e Classes em Models

---
+++ LanchesLinux/Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿namespace LanchesLinux.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        
+        public string Nome{ get; set; }
+        
+        public string Descrição{ get; set; }
+
+        public List<Lanche> Lanches { get; set;}
+    }
+}
---
+++ LanchesLinux/Models/Lanche.cs
@@ -0,0 +1,26 @@
+﻿namespace LanchesLinux.Models
+{
+    public class Lanche
+    {
+        public int LancheId { get; set; }
+
+        public string Nome { get; set; }
+        
+        public string DescriçãoCurta { get; set; }
+        
+        public string DescriçãoDetalhada { get; set; }
+        
+        public decimal Preço { get; set; }
+        
+        public string ImagemUrl { get; set; }
+        
+        public string ImagemThumbnailUrl { get; set; }
+        
+        public bool IsLanchePreferido{ get; set; }
+        
+        public bool EmEstoque { get; set; }
+
+        public int CategoriaId { get; set; }
+        public virtual Categoria Categoria { get; set; }
+    }
+}
